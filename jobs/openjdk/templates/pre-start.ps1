a13f3e45e5fc631caafc29bd039dac5e312c35dd
Sai Boorlagadda <sboorlagadda@pivotal.io>
2018-07-16 07:11:33 -0700

    Print out full environment variables
    Signed-off-by: Jens Deppe <jdeppe@pivotal.io>

--- jobs/openjdk/templates/pre-start.ps1
+++ jobs/openjdk/templates/pre-start.ps1
@@ -1,6 +1,6 @@
-﻿$ErrorActionPreference = "Stop";
+$ErrorActionPreference = "Stop";
 trap { $host.SetShouldExit(1) }
 
-gci env:* | sort-object name
+gci env:* | sort-object name | format-list
 
 c:\var\vcap\packages\chocolatey\tools\chocolateyInstall\choco.exe install -y jdk8
